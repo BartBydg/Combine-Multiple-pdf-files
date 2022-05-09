e1b0cbd5c3806d90cc6c987a58d28fb8a00bf43a
Bartosz Rafiński <bartosz.rafinski@gmail.com>
2022-05-09 20:23:43 +0200

    Add class for pdf operations

---
+++ Combine Multiple pdf files/PdfSupport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Combine_Multiple_pdf_files
+{
+    class PdfSupport
+    {
+    }
+}
